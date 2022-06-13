b94e90a73e976f86e466271be7fcb266f93dbaaa
Honesh Rai Pareek <honeshraipareek@gmail.com>
2022-06-13 16:55:54 +0530

    hope

---
+++ index.js
@@ -0,0 +1,10 @@
+//Function for Side bar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "350px";
+    document.getElementById("main").style.marginLeft = "350px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
